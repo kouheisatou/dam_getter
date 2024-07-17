79aea1d27003d8a0c91214ea4d864aa8acd513d7
kouheisatou <koheisato0420@gmail.com>
2024-07-18 00:11:04 +0900

    ADD : save dam_id and dam_password

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
-import 'package:dam_getter/history_screen.dart';
-import 'package:dam_getter/values_secret.dart';
 import 'package:dam_getter/values.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -12,8 +9,9 @@ class LoginScreen extends StatefulWidget {
 
   // String username = "";
   // String password = "";
-  String username = SAMPLE_USER;
-  String password = SAMPLE_PASS;
+
+  TextEditingController damID = TextEditingController();
+  TextEditingController damPassword = TextEditingController();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
@@ -28,76 +26,92 @@ document.getElementById('LoginButton').click();
   }
 
   @override
+  void initState() {
+    setSavedUserAndPass();
+    super.initState();
+  }
+
+  Future<void> setSavedUserAndPass() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      widget.damID.text = prefs.getString("dam_id") ?? "";
+      widget.damPassword.text = prefs.getString("dam_password") ?? "";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: InAppWebView(
-              initialUrlRequest: URLRequest(url: WebUri(DAM_MYPAGE_URL)),
-              onWebViewCreated: (InAppWebViewController controller) {
-                webView = controller;
-              },
-              onLoadStop: (controller, url) {
-                if (url.toString() == DAM_LOGIN_SUCCEEDED_PAGE) {
-                  Navigator.pop(context);
-                }
-              },
-            ),
-          ),
-          Positioned.fill(
-            child: Container(
-              color: Theme.of(context).colorScheme.surface,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: TextFormField(
-                      initialValue: widget.username,
-                      textAlign: TextAlign.center,
-                      decoration: const InputDecoration(
-                        isCollapsed: true,
-                        contentPadding: EdgeInsets.zero,
-                        border: InputBorder.none,
-                        hintText: "D A M - I D",
-                      ),
-                      onChanged: (value) {
-                        widget.username = value;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: TextFormField(
-                      initialValue: widget.password,
-                      textAlign: TextAlign.center,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        isCollapsed: true,
-                        contentPadding: EdgeInsets.zero,
-                        border: InputBorder.none,
-                        hintText: "P A S S W O R D",
-                      ),
-                      onChanged: (value) {
-                        widget.password = value;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 100),
-                    child: IconButton(
-                      onPressed: () async {
-                        await login(widget.username, widget.password);
-                      },
-                      icon: const Icon(Icons.login),
-                    ),
-                  ),
-                ],
+      body: GestureDetector(
+        onTap: () async {
+          primaryFocus?.unfocus();
+        },
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(url: WebUri(DAM_MYPAGE_URL)),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  webView = controller;
+                },
+                onLoadStop: (controller, url) {
+                  if (url.toString() == DAM_LOGIN_SUCCEEDED_PAGE) {
+                    Navigator.pop(context);
+                  }
+                },
               ),
             ),
-          ),
-        ],
+            Positioned.fill(
+              child: Container(
+                color: Theme.of(context).colorScheme.surface,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextFormField(
+                        controller: widget.damID,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          isCollapsed: true,
+                          contentPadding: EdgeInsets.zero,
+                          border: InputBorder.none,
+                          hintText: "D A M - I D",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextFormField(
+                        controller: widget.damPassword,
+                        textAlign: TextAlign.center,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          isCollapsed: true,
+                          contentPadding: EdgeInsets.zero,
+                          border: InputBorder.none,
+                          hintText: "P A S S W O R D",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 50, 0, 100),
+                      child: IconButton(
+                        onPressed: () async {
+                          await login(widget.damID.text, widget.damPassword.text);
+                          final prefs = await SharedPreferences.getInstance();
+                          await prefs.setString("dam_id", widget.damID.text);
+                          await prefs.setString("dam_password", widget.damPassword.text);
+                        },
+                        icon: const Icon(Icons.login),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
