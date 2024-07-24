412d0876b01cce364b23f42dd4bd6eb9e2d62e62
kouheisatou <koheisato0420@gmail.com>
2024-07-24 18:47:20 +0900

    ADD : score_detail_screen.dart

--- lib/scoreListItem.dart
+++ lib/scoreListItem.dart
@@ -1,4 +1,5 @@
 import 'package:dam_getter/score_data_model.dart';
+import 'package:dam_getter/score_detail_screen.dart';
 import 'package:dam_getter/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -21,53 +22,66 @@ class ScoreDataListItem extends StatelessWidget {
         ),
         Expanded(
           child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
+            child: InkWell(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ScoreDetailScreen(score);
+                    },
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              score.contentsName,
+                              style: TextStyle(fontSize: 15, color: Theme.of(context).colorScheme.onSurface),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              score.artistName,
+                              style: TextStyle(fontSize: 10, color: Theme.of(context).colorScheme.onSurface),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            score.contentsName,
-                            style: TextStyle(fontSize: 15, color: Theme.of(context).colorScheme.onSurface),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Text(
-                            score.artistName,
-                            style: TextStyle(fontSize: 10, color: Theme.of(context).colorScheme.onSurface),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                          Row(
+                            children: [
+                              Baseline(
+                                baseline: 20.0,
+                                baselineType: TextBaseline.alphabetic,
+                                child: Text(score.score.toString().split(".")[0], style: const TextStyle(fontSize: 20, fontFeatures: [FontFeature.tabularFigures()])),
+                              ),
+                              Baseline(
+                                baseline: 15.8,
+                                baselineType: TextBaseline.alphabetic,
+                                child: Text(".${score.score.toStringAsFixed(3).split(".")[1]}", style: const TextStyle(fontSize: 10, fontFeatures: [FontFeature.tabularFigures()])),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Baseline(
-                              baseline: 20.0,
-                              baselineType: TextBaseline.alphabetic,
-                              child: Text(score.score.toString().split(".")[0], style: const TextStyle(fontSize: 20, fontFeatures: [FontFeature.tabularFigures()])),
-                            ),
-                            Baseline(
-                              baseline: 15.8,
-                              baselineType: TextBaseline.alphabetic,
-                              child: Text(".${score.score.toStringAsFixed(3).split(".")[1]}", style: const TextStyle(fontSize: 10, fontFeatures: [FontFeature.tabularFigures()])),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
---
+++ lib/score_detail_screen.dart
@@ -0,0 +1,119 @@
+import 'package:dam_getter/score_data_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:xml/xml.dart' as xml;
+import 'package:xml/xml.dart';
+
+class ScoreDetailScreen extends StatelessWidget {
+  ScoreDetailScreen(this.scoreData);
+
+  ScoreDataModel scoreData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('スコア詳細'),
+      ),
+      body: Scrollbar(
+        child: ListView(
+          children: [
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: Text("基本データ", style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Table(
+                children: [
+                  _buildTableRow(context, 'ID', scoreData.id),
+                  _buildTableRow(context, 'Score Type', scoreData.scoreType.toString().split('.').last),
+                  _buildTableRow(context, 'Contents Name', scoreData.contentsName),
+                  _buildTableRow(context, 'Artist Name', scoreData.artistName),
+                  _buildTableRow(context, 'Score', scoreData.score.toString()),
+                  _buildTableRow(context, 'Score Average', scoreData.scoreAverage.toString()),
+                  _buildTableRow(context, 'Scoring Time', scoreData.scoringTime.toString()),
+                ],
+              ),
+            ),
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: Text("詳細データ", style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Table(
+                children: convertAttributesToMap(scoreData.xml).entries.map((entry) => _buildTableRow(context, entry.key, entry.value)).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Map<String, String> convertAttributesToMap(String xmlString) {
+    final document = XmlDocument.parse(xmlString);
+    final firstElement = document.rootElement;
+    final attributesMap = <String, String>{};
+    for (final attribute in firstElement.attributes) {
+      attributesMap[attribute.name.toString()] = attribute.value;
+    }
+    return attributesMap;
+  }
+
+  TableRow _buildTableRow(BuildContext context, String field, String value) {
+    return TableRow(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(field),
+                ),
+              );
+            },
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                field,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 10),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(value),
+                ),
+              );
+            },
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                value,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
